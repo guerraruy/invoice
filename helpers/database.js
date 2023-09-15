2cdf6ab69537d08f5e255c51331449aad46bca65
“guerraruy” <ruyguerra.dev@gmail.com>
2023-09-15 20:37:09 -0300

    Fix for env variables

--- helpers/database.js
+++ helpers/database.js
@@ -2,8 +2,7 @@ import { MongoClient } from 'mongodb'
 import { ObjectId } from 'mongodb'
 
 export const getDbConnectionString = () => {
-  const { database, username, cluster, password } = process.env.db
-  return `mongodb+srv://${username}:${password}@${cluster}.qvebhze.mongodb.net/${database}?retryWrites=true&w=majority`
+  return `mongodb+srv://${process.env.dbUsername}:${process.env.dbPassword}@${process.env.dbCluster}.qvebhze.mongodb.net/${process.env.dbDatabase}?retryWrites=true&w=majority`
 }
 
 export const dbConnect = async () => {
